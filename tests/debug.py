1b9a188ea50045fb5340753a7d617a65093f5a92
peterdeweirdt <petedeweirdt@gmail.com>
2024-11-01 16:09:39 -0400

    setup: setup license and github actions

--- tests/debug.py
+++ tests/debug.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from pathlib import Path
+import os
 
 if __name__ == '__main__':
+    os.system('ls -l tests/data')
     true_y_path = Path(__file__).parent / 'data' / 'GCF_003333385_beaker_predictions.csv'
     true_y_df = pd.read_csv(true_y_path, index_col=0)
     print('Head of true Y df:', true_y_df.head())
