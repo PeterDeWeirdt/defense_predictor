ff305492e8bd95357b00992d67831df22da1af07
peterdeweirdt <petedeweirdt@gmail.com>
2024-11-01 16:00:47 -0400

    setup: setup license and github actions

---
+++ tests/debug.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from pathlib import Path
+
+if __name__ == '__main__':
+    true_y_path = Path(__file__).parent / 'data' / 'GCF_003333385_beaker_predictions.csv'
+    true_y_df = pd.read_csv(true_y_path, index_col=0)
+    print('Head of true Y df:', true_y_df.head())
+    print('Columns of true Y df:', true_y_df.columns)
+    true_y_df = true_y_df[['beaker_prediction']]
+    print('Head of true Y df:', true_y_df.head())
