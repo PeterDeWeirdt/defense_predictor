71e0ff567df84234bec1f5b3edc5912700d6c1e8
peterdeweirdt <petedeweirdt@gmail.com>
2025-10-03 11:50:07 -0400

    code: update to DefensePredictor v1

--- defense_predictor/downloads.py
+++ defense_predictor/downloads.py
@@ -4,52 +4,36 @@ import os
 import gzip
 
 
-def download_model_weights():
-    url = "https://zenodo.org/records/16937977/files/beaker_v3.pkl.gz?download=1"
-    gz_path = os.path.join(os.path.dirname(__file__), "beaker_v3.pkl.gz")
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(gz_path, "wb") as f:
-            shutil.copyfileobj(r.raw, f)
-    with gzip.open(gz_path, 'rb') as gz_file:
-        with open(os.path.join(os.path.dirname(__file__), "beaker_v3.pkl"), 'wb') as out_file:
-            shutil.copyfileobj(gz_file, out_file)
-    os.remove(gz_path)
-
-
-def download_esm2_contact_weights():
-    url = 'https://zenodo.org/records/16937977/files/esm2_t30_150M_UR50D-contact-regression.pt.gz?download=1'
-    gz_path = os.path.join(os.path.dirname(__file__), 'esm2_t30_150M_UR50D-contact-regression.pt.gz')
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(gz_path, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
-    with gzip.open(gz_path, 'rb') as gz_file:
-        with open(os.path.join(os.path.dirname(__file__), 'esm2_t30_150M_UR50D-contact-regression.pt'), 'wb') as out_file:
-            shutil.copyfileobj(gz_file, out_file)
-    os.remove(gz_path)
-
-
-def download_esm2_weights():
-    url = 'https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t30_150M_UR50D.pt'
-    path = os.path.join(os.path.dirname(__file__), 'esm2_t30_150M_UR50D.pt')
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(path, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
+def download_file_weights(file, url):
+    out_path = os.path.join(os.path.dirname(__file__), file)
+    try:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(out_path, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+    except:
+        print(f"Could not download {file}, you can try manually downloading {file} from {url} and placing it in this directory {out_path}")
 
 
 def download_weights():
+    download_dict = {'beaker_fold_0.pkl': 'https://zenodo.org/records/17258204/files/beaker_fold_0.pkl?download=1',
+                     'beaker_fold_1.pkl': 'https://zenodo.org/records/17258204/files/beaker_fold_1.pkl?download=1',
+                     'beaker_fold_2.pkl': 'https://zenodo.org/records/17258204/files/beaker_fold_2.pkl?download=1',
+                     'beaker_fold_3.pkl': 'https://zenodo.org/records/17258204/files/beaker_fold_3.pkl?download=1',
+                     'beaker_fold_4.pkl': 'https://zenodo.org/records/17258204/files/beaker_fold_4.pkl?download=1',
+                     'esm2_t30_150M_UR50D-contact-regression.pt': 'https://zenodo.org/records/17258204/files/esm2_t30_150M_UR50D-contact-regression.pt?download=1',
+                     'esm2_t30_150M_UR50D.pt': 'https://zenodo.org/records/17258204/files/esm2_t30_150M_UR50D.pt?download=1'}
+    required_files = list(download_dict.keys())
     base_dir = os.path.dirname(__file__)
-    downloaded_weights = os.listdir(base_dir)
-    if "beaker_v3.pkl" not in downloaded_weights:
-        print("Downloading model weights")
-        download_model_weights()
-    if "esm2_t30_150M_UR50D-contact-regression.pt" not in downloaded_weights:
-        download_esm2_contact_weights()
-    if "esm2_t30_150M_UR50D.pt" not in downloaded_weights:
-        print("Downloading ESM2 weights")
-        download_esm2_weights()
+    base_dir_files = os.listdir(base_dir)
+    missing_weights = [x for x in required_files if x not in base_dir_files]
+    if len(missing_weights) == 0:
+        print("All weights have been downloaded")
+    else:
+        print('Downloading model weights...')
+        for k in missing_weights:
+            print(f"Downloading {k}")
+            download_file_weights(k, download_dict[k])
 
 
 if __name__ == "__main__":
